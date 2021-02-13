8a570277e5e55f5146cfe650d686fbddaa813f28
Ksenia serdyukova <kingsbroo@gmail.com>
2021-02-13 00:26:33 -0400

    commit tree 1ede1b03a9a765250187e3f6362b7bc80ab86e1e

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+
+def line(katz_deli)
+    if katz_deli.empty?
+    puts "The line is currently empty."
+    else 
+        current_line = "The line is currently:"
+        katz_deli.each.with_index(1) do |name, num|
+          current_line << " #{num}. #{name}"
+        end
+        puts current_line
+      end
+    
+
+
+end
